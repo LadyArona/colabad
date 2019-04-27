a6f50f3702caf2e5307d775e6614d2040a918ff7
talliny <tallinydn@gmail.com>
2019-04-27 15:28:38 -0300

    alteracoes

--- application/controllers/Emails.php
+++ application/controllers/Emails.php
@@ -5,20 +5,12 @@ class Emails extends CI_Controller {
 
   public function __construct() {
       parent::__construct();
-      $this->load->model('Emails_model', 'Emails_model');
+      $this->load->model('Email_model', 'email');
   }
 
   public function index(){
-      redirect(base_url().'login');
+
   }    
-  
-  function _remap($method){
-    if (method_exists($this, $method) && $_SERVER['REQUEST_METHOD'] == 'POST'){
-      $this->$method();
-    }else {
-      $this->index($method);
-    }
-  }
 
   function verificar_email($verifica = null) {  
     $registro = $this->Emails_model->verifica_email($verifica);
@@ -34,8 +26,8 @@ class Emails extends CI_Controller {
   }
 
   function envia_verificacao() {  
-    $this->Emails_model->envia_verificacao("yashwantchavan@technicalkeeda.com","13nRGi7UDv4CkE7JHP1o");
-    $this->load->view('index.php', $data);   
-  } 
+    $this->email->envia_verificacao("loja.anima.animus@gmail.com","13nRGi7UDv4CkE7JHP1o");
+    redirect(base_url(), 'refresh');
+  }
 }
 ?>
--- application/models/Email_model.php
+++ application/models/Email_model.php
@@ -1,19 +1,22 @@
-<?php
-  class Email_model extends Model {
-    function Email_model(){
-      parent::Model();
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Email_model extends CI_Model {
+
+    public function __construct(){
+      parent::__construct();
     }
 
-    function envia_verificacao($email, $html){
-      $html = "Dear User,\nPlease click on below URL or paste into your browser to verify your Email Address\n\n http://www.yourdomain.com/verify/".$verificationText."\n"."\n\nThanks\nAdmin Team";
+    function envia_verificacao($para, $verificacao){
+      $html = "Dear User,\nPlease click on below URL or paste into your browser to verify your Email Address\n\n http://www.yourdomain.com/verify/".$verificacao."\n"."\n\nThanks\nAdmin Team";
 
       $config = 
         array(
           'protocol'  => 'smtp',
-          'smtp_host' => 'smtp.yourdomain.com.',
-          'smtp_port' => 465,
-          'smtp_user' => 'admin@yourdomain.com', // change it to yours
-          'smtp_pass' => '########', // change it to yours
+          'smtp_host' => 'smtp.gmail.com',
+          'smtp_port' => 587,
+          'smtp_user' => 'tallinydn@gmail.com', // change it to yours
+          'smtp_pass' => '022189Adn', // change it to yours
           'mailtype'  => 'html',
           'charset'   => 'iso-8859-1',
           'wordwrap'  => TRUE
@@ -22,11 +25,13 @@
       $this->load->library('email', $config);
 
       $this->email->set_newline("\r\n");
-      $this->email->from('admin@yourdomain.com', "Admin Team");
-      $this->email->to($email);  
+      $this->email->from('tallinydn@gmail.com', "Talliny ColabAD");
+      $this->email->to($para);  
       $this->email->subject("Email Verification");
       $this->email->message($html);
       $this->email->send();
+
+      echo "foi";
     }
 
     function verifica_email($codigo){  
